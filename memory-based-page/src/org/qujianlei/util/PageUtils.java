28f1a566c69dee6e71b0eb5e22df1acd968efcfd
MrQuJL <909301740@qq.com>
2018-10-16 22:43:29 +0800

    build a project

---
+++ memory-based-page/src/org/qujianlei/util/PageUtils.java
@@ -0,0 +1,14 @@
+package org.qujianlei.util;
+
+/**
+ * @author 曲健磊
+ * @date 2018年10月16日 下午10:41:12
+ * @description 假分页的主要实现类
+ */
+public class PageUtils {
+
+    public static Page<E> startPage() {
+        
+    }
+    
+}
